03ce05f7f08f05b1a95d1cdded885bab717866dc
sw <sw@example.com>
2025-12-29 01:01:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    hi there!
+    <div>
+      <button @click="onTest">測試</button>
+      <span>Clicked: {{ count }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message: 'Hello Vue 2!',
+      count: 0
+    }
+  },
+  methods: {
+    onTest() {
+      this.count++
+      
+      console.log('測試 button clicked', this.count)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
