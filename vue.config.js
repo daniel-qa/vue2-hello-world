69e1dd152933c04251e85b8e72aa106cb04c9861
sw <sw@example.com>
2026-01-02 09:37:41 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  // 這行最重要，請確保有這一行
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue2-hello-world/'
+    : '/',
+  
   transpileDependencies: true,
   configureWebpack: config => {
     if (process.env.NODE_ENV !== 'production') {
